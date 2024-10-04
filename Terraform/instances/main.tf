2c1f8a56f12cc189c473e819981559e00cdd461e
anjali0497 <129653598+anjali0497@users.noreply.github.com>
2024-10-04 13:12:31 +0530

    Update main.tf

--- Terraform/instances/main.tf
+++ Terraform/instances/main.tf
@@ -7,16 +7,6 @@ resource "aws_instance" "Redis-public" {
   security_groups = [var.public-sg-id]
   key_name = var.key-name
 
-   # User Data to add public SSH key to the instance
-  user_data = <<-EOF
-              #!/bin/bash
-              mkdir -p /home/ubuntu/.ssh
-              echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCUBnJlQOlvJtJbLVW6zU+s5SlfO8rXz1wPrR3eMLCYeRSpFZlGeTJlBK9MzyzfNB5+iUEMVXQo6AaJcQn8BMzhG0sWqu+ROLJgOaxF+BYRs10mkeHNwGypzskSFPQoTvgW0pI/FvHt/0T4//bpKLsugvSHcTApxb5TGy2D3atd+qymo9oC0Z/fNAr73xHrBCboWPNfsXHVT/GAicfG6sIgmtTJNEubrWghVTbmYIzS4hYgcjMGfFENYcc5Cp0NPlU4v2k6BrkZjYIwUqJUx829F18XiWbXHOMZLlBY80cGNXWz1+hmsjuACau5SQ8kMaMjMFK+dFR7krwlmMg1e15/ sydney-key" >> /home/ubuntu/.ssh/authorized_keys && \
-              chmod 700 /home/ubuntu/.ssh && \
-              chmod 600 /home/ubuntu/.ssh/authorized_keys && \ 
-              chown -R ubuntu:ubuntu /home/ubuntu/.ssh
-              EOF
-
   tags = {
     Name = "Redis-public"
   }

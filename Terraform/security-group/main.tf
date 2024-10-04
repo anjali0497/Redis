64e27df9d4e1a4a5924784ebb5829adfcd642345
anjali0497 <129653598+anjali0497@users.noreply.github.com>
2024-10-04 11:34:32 +0530

    Update main.tf

--- Terraform/security-group/main.tf
+++ Terraform/security-group/main.tf
@@ -83,7 +83,7 @@ resource "aws_security_group" "private-SG" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.1.0/24"]
   }
 
   # Ingress rule for ICMP (ping) from the default VPC security group

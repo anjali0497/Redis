87ebd1c874073cfbca6b37ef65b9f8250cc6c7da
anjali0497 <129653598+anjali0497@users.noreply.github.com>
2024-10-04 11:26:33 +0530

    Update main.tf

--- Terraform/vpc-peering/main.tf
+++ Terraform/vpc-peering/main.tf
@@ -28,7 +28,7 @@ data "aws_route_table" "default_RT" {
 
 resource "aws_route" "default_rt" {
 route_table_id = data.aws_route_table.default_RT.id
-destination_cidr_block = var.vpc-cidr
+destination_cidr_block = var.vpc_cidr
 vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
 }
 
@@ -39,35 +39,3 @@ data "aws_security_group" "default_sg" {
     values = [ "default" ]
   }
 }
-
-
-
-
-
-
-/*
-
-# Route traffic from the custom VPC to the default VPC
-resource "aws_route" "custom_to_default" {
-  route_table_id         = aws_vpc.Redis-VPC.main_route_table_id
-  destination_cidr_block = data.aws_vpc.default_vpc.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-# Create a VPC peering connection between the default VPC and custom VPC
-resource "aws_vpc_peering_connection" "peer" {
-  vpc_id        = data.aws_vpc.default_vpc.id         # Default VPC
-  peer_vpc_id   = aws_vpc.Redis-VPC.id           # Custom VPC
-  auto_accept   = true  # Automatically accept the peering request
-
-  tags = {
-    Name = "Default-to-Custom-VPC-Peering"
-  }
-}
-
-# Route traffic from the default VPC to the custom VPC
-resource "aws_route" "default_to_custom" {
-  route_table_id         = data.aws_vpc.default_vpc.main_route_table_id
-  destination_cidr_block = aws_vpc.Redis-VPC.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}*/
